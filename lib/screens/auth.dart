1cbb580366bf10a6d00f5d84fdfdd26ce28d64eb
AryanShr <aryanshr91003@gmail.com>
2024-08-02 00:46:47 +0530

    Fixed email validation

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -175,7 +175,7 @@ class _AuthScreenState extends ConsumerState<AuthScreen> {
                           validator: (value) {
                             if (value == null ||
                                 value.trim().isEmpty ||
-                                !value.contains('@')) {
+                                !RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value)) {
                               return 'Please enter a valid email address';
                             }
 
